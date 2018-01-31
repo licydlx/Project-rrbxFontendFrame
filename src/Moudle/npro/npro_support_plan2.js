e48ea5cd87592ba5b6dac2c05f046cdf43e1efb0
ydlx <lichong536@163.com>
2018-01-31 16:24:53 +0800

    剔除 多余文件

--- src/Moudle/npro/npro_support_plan2.js
+++
@@ -1,6 +0,0 @@
-require("../../Static/scss/component/npro/npro_support_plan2.scss");
-import ejs from '../../Static/ejs/npro/npro_support_plan2.ejs';
-import e_npro_support_plan2 from '../../Static/js/npro/npro_support_plan2.js';
-
-const npro_support_plan2 = (par) => ejs(par);
-export default npro_support_plan2;
--- src/Moudle/npro/npro_support_plan3.js
+++
@@ -1,6 +0,0 @@
-require("../../Static/scss/component/npro/npro_support_plan3.scss");
-import ejs from '../../Static/ejs/npro/npro_support_plan3.ejs';
-import e_npro_support_plan3 from '../../Static/js/npro/npro_support_plan3.js';
-
-const npro_support_plan3 = (par) => ejs(par);
-export default npro_support_plan3;
