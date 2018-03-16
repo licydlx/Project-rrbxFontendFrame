8ae524ab87b4c319ee172e688dfefc70645797fe
ydlx <lichong536@163.com>
2018-03-16 14:46:36 +0800

    20170905zhaoshangzawl pro1.0.1

---
+++ src/Config/Page/20180312anxinbaoxiankaws/npro/dataFlow.js
@@ -0,0 +1,65 @@
+const dataFlow = function(data) {
+	// GV:全局变量; insuredPars:试算及投保参数; renderDate: 试算及投保页面数据
+	// 作者:ydlx
+	// 日期:2018-3-13
+	localStorage.setItem(data.productId, JSON.stringify({
+		'GV': GV,
+		'insuredPars': {
+			"pars": {
+				"rrbx": {
+					"rrbxProductId": data.productId,
+					"productSeriesId": data.value.insurancePlan[0].id,
+					"periodPremium": data.value.insurancePlan[0].price,
+					"buyNum": 1,
+					"insurantApplicantRelation": "03"
+				},
+				"extraParams": {
+					"holderOccupationCode": "",
+					"holderProvince": "",
+					"holderCity": "",
+					"holderDistrict": "",
+
+					"holderBirthday":"1992-02-02",
+					"insuredBirthday":"2017-02-02",
+					"payEndYear": "5",
+					"payIntv": "1",
+					"ylAmnt": "50",
+					"sex":"men",
+					"riskCodes": "QMLH001",
+					"amnt": "5",
+					"prem": "608.72"
+				}
+			},
+			"parsInit": {
+				"rrbx": {
+					"rrbxProductId": data.productId,
+					"productSeriesId": data.value.insurancePlan[0].id,
+					"periodPremium": data.value.insurancePlan[0].price,
+					"buyNum": 1,
+					"insurantApplicantRelation": "03"
+				},
+				"extraParams": {
+					"holderOccupationCode": "",
+					"holderProvince": "",
+					"holderCity": "",
+					"holderDistrict": "",
+
+					"holderBirthday":"1992-02-02",
+					"insuredBirthday":"2017-02-02",
+					"payEndYear": "5",
+					"payIntv": "1",
+					"ylAmnt": "50",
+					"sex":"men",
+					"riskCodes": "QMLH001",
+					"amnt": "5",
+					"prem": "608.72"
+				}
+			}
+		},
+		"renderDate": {
+			'insurePolicy': data.value.insurePolicy,
+			"insurancePlan": null
+		}
+	}));
+}
+export default dataFlow;
---
+++ src/Config/Page/20180312anxinbaoxiankaws/ntri/serviceLogic.js
@@ -0,0 +1,138 @@
+import {
+	consultServie,
+	dateModal
+} from '../../../../Static/js/common/modal.js';
+
+import premAjax from '../../../../Static/js/depend/datas/premAjax.js';
+import selectDate from '../../../../Static/js/depend/tools/selectDate.js';
+import selectOne from '../../../../Static/js/depend/tools/selectOne.js';
+import dateUnit from '../../../../Static/js/depend/tools/dateUnit.js';
+
+const serviceLogic = function(a) {
+	var renderData = a[0],
+		rrbxSetObj = a[1];
+	// 初始化 保費參數值(由投保页回退回来) (多层级 对象 深拷贝)
+	var parsObj = JSON.parse(JSON.stringify(rrbxSetObj.insuredPars.parsInit));
+	getPrem(rrbxSetObj);
+	// 客服咨询
+	new consultServie("consultService", "#service", "#service-pop").init();
+	// =============================
+
+	// 投保人出生日期
+	new selectDate($("#holderBirthday"), "birthday", '1992-02-02', 60, -20, holderBirthday).init();
+
+	function holderBirthday(value) {
+		var flag = dateUnit.getAgeRangeState(value,{"age":20},{"age":60});
+
+		if (!flag) {
+			new dateModal(null,"stateIndform","投保人年龄最小20岁，最大60岁").init().show();
+			return false;
+		} else {
+			parsObj.extraParams.holderBirthday = value;
+			getPrem(rrbxSetObj);
+			return true;
+		};
+	}
+	// 被保人出生日期
+	new selectDate($("#insuredBirthday"), "birthday", '2017-02-02', 9, 0, insuredBirthday).init();
+
+	function insuredBirthday(value) {
+		var flag = dateUnit.getAgeRangeState(value,{"ageDay":60},{"age":9});
+
+		if (!flag) {
+			new dateModal(null,"stateIndform","被保人年龄最小60天，最大9岁").init().show();
+			return false;
+		} else {
+			parsObj.extraParams.insuredBirthday = value;
+			getPrem(rrbxSetObj);
+			return true;
+		};
+	}
+	// 被保人性别
+	new selectOne($("#sex"), "性别选择", renderData.sex, sex).init();
+
+	function sex(value) {
+		parsObj.extraParams.sex = value;
+		getPrem(rrbxSetObj);
+	}
+
+	// 保额选择
+	new selectOne($("#amnt"), "保额选择", renderData.amnt, coverageFunc).init();
+
+	function coverageFunc(value) {
+		parsObj.extraParams.amnt = value;
+		getPrem(rrbxSetObj);
+	}
+
+	// 缴费年限
+	new selectOne($("#payEndYear"), "缴费年限", renderData.payEndYear, payEndYear).init();
+
+	function payEndYear(value) {
+		parsObj.extraParams.payEndYear = value;
+		getPrem(rrbxSetObj);
+	}
+
+	// 缴费频率
+	new selectOne($("#payIntv"), "缴费频率", renderData.payIntv, payIntv).init();
+
+	function payIntv(value) {
+		parsObj.extraParams.payIntv = value;
+		getPrem(rrbxSetObj);
+	}
+
+	// 住院津贴
+	new selectOne($("#ylAmnt"), "住院津贴", renderData.ylAmnt, ylAmnt).init();
+
+	function ylAmnt(value) {
+		parsObj.extraParams.ylAmnt = value;
+		getPrem(rrbxSetObj);
+	}
+
+	// 数组 原型 增加 remove 方法
+	// 作者:ydlx
+	// 日期:2018-3-13
+	Array.prototype.indexOf = function(val) {
+		for (var i = 0; i < this.length; i++) {
+			if (this[i] == val) return i;
+		}
+		return -1;
+	};
+	Array.prototype.remove = function(val) {
+		var index = this.indexOf(val);
+		if (index > -1) {
+			this.splice(index, 1);
+		}
+	};
+
+	// 附加险多选
+	var riskCodesArray = [parsObj.extraParams.riskCodes];
+	$("#riskCodes").on('click', 'a', function(event) {
+		event.preventDefault();
+		var that = $(this);
+		if (!that.hasClass("active")) {
+			that.addClass("active");
+			riskCodesArray.push(that.attr("data-id"));
+
+		} else {
+			that.removeClass("active");
+			riskCodesArray.remove(that.attr("data-id"));
+		}
+		parsObj.extraParams.riskCodes = riskCodesArray.join(',');
+
+		getPrem(rrbxSetObj);
+	});
+
+	// 获取保费 并 存储rrbxSet
+	function getPrem(rrbxSetObj) {
+		var ntriObj = parsObj.rrbx;
+		ntriObj["extraParams"] = parsObj.extraParams;
+
+		premAjax(ntriObj, function(value) {
+			$("#prem").text(value + "元");
+			parsObj.extraParams.prem = value;
+			rrbxSetObj.insuredPars.pars = parsObj;
+			localStorage.setItem(rrbxSetObj.insuredPars.parsInit.rrbx.rrbxProductId, JSON.stringify(rrbxSetObj));
+		});
+	}
+};
+export default serviceLogic;
